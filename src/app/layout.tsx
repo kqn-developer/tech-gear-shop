394049bc412de5db2e06897843282a6eef59351d
kqn-developer <khanh.qn.developer@gmail.com>
2025-03-07 23:23:06 +0700

    feat(ui): cập nhật metadata và thay đổi font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,24 @@
+import { Metadata } from 'next'
+import { Be_Vietnam_Pro } from 'next/font/google'
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Tech Gear',
+  description:
+    'Tech Gear - nền tảng thương mại điện tử chuyên cung cấp linh kiện máy tính, PC và laptop chính hãng với giá tốt, đa dạng sản phẩm, và dịch vụ hỗ trợ chuyên nghiệp',
+}
+
+const be_vietnam_pro = Be_Vietnam_Pro({
+  subsets: ['vietnamese'],
+  weight: ['400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='vi'>
-      <body>{children}</body>
+      <body className={be_vietnam_pro.className}>{children}</body>
     </html>
   )
 }
